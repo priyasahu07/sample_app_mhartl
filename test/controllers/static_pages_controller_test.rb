7676df6fd3562b5df3e1801b595b4e8d0d3c1df8
priyasahu <sahu.priya488@gmail.com>
2017-09-02 14:54:37 +0530

    add test and about page

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,19 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+     # assert_select "title", "Home | #{@title}"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    # assert_select "title" , "Help | #{@title}"
   end
 
 
